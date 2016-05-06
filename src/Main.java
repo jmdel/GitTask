a76403afe82cdf67f687bddf0fa79e6844b3cfc1
jean-michel <jean-michel@delorme.fr>
2016-05-06 11:28:12 +0200

    Commented

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
-
+	
 	private static Scanner scan;
+	//The fist 3 numbers have the INITIAL_VALUE=-99
 	private static final int INITIAL_VALUE=-99;
 	private static int first=INITIAL_VALUE;
 	private static int second=INITIAL_VALUE;
@@ -13,18 +14,18 @@ public class Main {
 		scan = new Scanner(System.in);
 		
 		int number = 0;
-		while (number != -1) {
+		while (number != -1) { //-1 is the value to exit
 			// New input
 			System.out.println("Entrez une nouvelle valeur :");
 			number = getInputNumber();
 			
-			if (number == -2) {
+			if (number == -2) {//-2 is the state value for invalid input
 				System.out.println("Valeur invalide");
 			}
-			else if (number == -1) {
+			else if (number == -1) { //Finish
 				displayEnd();
 			}
-			else {
+			else { //Sort and display the "podium"
 				sortBiggestNumbers(number);
 				displayBiggestNumbers();
 			}
@@ -32,6 +33,7 @@ public class Main {
 	}
 	
 	private static void sortBiggestNumbers(int number) {
+		//Set the 3 first positions
 		if (number > first) {
 			third = second;
 			second = first;
@@ -51,7 +53,7 @@ public class Main {
 	}
 
 	private static void displayBiggestNumbers() {
-		//Display the 3 biggest numbers
+		//Display the 3 biggest numbers, no display if INITIAL_VALUE
 		if (first!=INITIAL_VALUE){
 			System.out.println("Fisrt : "+first);
 		}
@@ -60,11 +62,11 @@ public class Main {
 		}
 		if (third!=INITIAL_VALUE){
 			System.out.println("Third : "+third);
-		}
-		//System.out.println("Affichage du résultat");		
+		}		
 	}
 
 	public static int getInputNumber() {
+		//The value of input scanner or a state exception value (-1 or -2)
 		int in = -2;
 		// Convert the keyboard input to int
 		try {
